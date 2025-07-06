80717ee2663c119d0c0e22af0204a7cde91c8054
chinchotren <andresgonzalez10.3@gmail.com>
2025-07-06 14:20:45 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_icp2ofAN4CIjyWMs1Vxne-HzTIKBkfhY7drGGhzb_c/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS3UxWfuvGtTYRAcHJR8XCUuWZGM-oPumUe-G88LVCv2Gyc-sZhLEL8nHdKhPDf5m1mVozDSvT-n7jE/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
